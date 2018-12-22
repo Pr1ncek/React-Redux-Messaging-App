86f14793476a8c1c98fc5eb0bd60eebf9f592eee
Prince K <k.prince1997@gmail.com>
2018-12-22 10:17:43 -0800

    Set up firebase and router

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyA8kk8IoC1ixA64Vc5R6jqS4q3cQi79nkw',
+  authDomain: 'react-messenger-f0469.firebaseapp.com',
+  databaseURL: 'https://react-messenger-f0469.firebaseio.com',
+  projectId: 'react-messenger-f0469',
+  storageBucket: 'react-messenger-f0469.appspot.com',
+  messagingSenderId: '538333283822'
+};
+firebase.initializeApp(config);
+
+export default firebase;
